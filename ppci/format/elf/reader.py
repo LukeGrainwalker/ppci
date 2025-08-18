13427b181b39ee509a22f213c15d55623cc14911
LukeGrainwalker <l-p-koerner@gmx.de>
2025-08-18 12:53:16 +0200

    some syntax fixes

--- ppci/format/elf/reader.py
+++ ppci/format/elf/reader.py
@@ -1,7 +1,7 @@
 """Support to process an ELF file."""
 
 from .file import ElfFile
-from .../binutils/objectfile.py import ObjectFile, Section
+from .headers import SymbolTableBinding, SymbolTableType
 
 # TODO: move some parts from ElfFile to this file.
 
@@ -10,11 +10,14 @@ def read_elf(f):
     """Read an ELF file"""
     return ElfFile.load(f)
 
+
 def elf_to_object(f):
+    from ppci.binutils.objectfile import ObjectFile, Section, RelocationEntry
+
     obj = ObjectFile(f.e_machine.aname.lower())
     if f.elf_header.e_entry != 0:
         obj.entry_symbol_id = f.elf_header.e_entry
-    
+
     for s in f.sections:
         so = Section(s.name)
         obj.add_section(so)
@@ -24,26 +27,28 @@ def elf_to_object(f):
 
     for r in relocations:
         if self.header.r_addend:
-            addend = self.header.r_addend)
+            addend = self.header.r_addend
         else:
             addend = 0
-        obj.add_relocation(RelocationEntry(
-            r.type,
-            r.symbol_id,
-            r.section,
-            r.header.r_offset,
-            addend,
-        ))
+        obj.add_relocation(
+            RelocationEntry(
+                r.type,
+                r.symbol_id,
+                r.section,
+                r.header.r_offset,
+                addend,
+            )
+        )
 
     if hasattr(f, "symbole_table"):
         for sym in f.symbole_table:
-           obj.add_symbol(
-                   sym.i,
-                   sym.name,
-                   SymbolTableBinding(sym.binding).name.lower(),
-                   sym.header.st_value,
-                   sym.section.name,
-                   SymbolTableType(sym.type).name.lower()
-                   sym.header.size,
-           )
+            obj.add_symbol(
+                sym.i,
+                sym.name,
+                SymbolTableBinding(sym.binding).name.lower(),
+                sym.header.st_value,
+                sym.section.name,
+                SymbolTableType(sym.type).name.lower(),
+                sym.header.size,
+            )
     return obj
