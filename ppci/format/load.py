3050eb0345341d6516c8363059cbefdd6779d844
LukeGrainwalker <l-p-koerner@gmx.de>
2025-10-06 09:42:05 +0200

    completed the loading function

--- ppci/format/load.py
+++ ppci/format/load.py
@@ -1,4 +1,5 @@
 from ppci.binutils import ObjectFile, deserialise
+from elf.reader import elf_to_object
 from io import FormatError
 import json
 
@@ -12,10 +13,13 @@ def load_object_file(file):
     file.seek(0)
     if b"ELF" in start:
         # read elf
+       return elf_to_object(file) 
     else:
+        # assume an json file
         data = file.read().decode()
         if data.isprintable():
             return deserialise(json.loads(data))
         else:
+            # Ok not a json file
             raise FormatError("file format not recognised")
     
